1efc38b864676c545c17600754902f1ee2e0e718
uppu.phanisai@gmail.com <uppu.phanisai@gmail.com>
2020-09-19 07:25:20 +0530

    set

--- 05_Wipro/ClassWork/src/com/mettle/SimpleEncodedArray.java
+++ 05_Wipro/ClassWork/src/com/mettle/SimpleEncodedArray.java
@@ -1,7 +1,8 @@
 package com.mettle;
 
 /**
- * original array : 2, 5, 1, 7, 9, 3 encoded array : 7, 6, 8, 16, 12, 3
+ * 2 5 1 7 9
+ *  encoded array : 7, 6, 8, 16, 12, 3
  * 
  * NOTE: Last element remains unchanged.
  */
@@ -19,21 +20,21 @@ public class SimpleEncodedArray {
         }
     }
 
-    public Result findOriginalFirstAndSum(int[] input1, int input2) {
+    public Result findOriginalFirstAndSum(int[] arr, int n) {
 
         int sum = 0;
-        int[] originalArray = new int[input2];
-        originalArray[input2 - 1] = input1[input2 - 1];
+        int[] originalArray = new int[n];
+        originalArray[n - 1] = arr[n - 1];
 
-        for (int i = input2 - 2; i > -1; i--) {
-            originalArray[i] = input1[i] - originalArray[i + 1];
+        for (int i = n - 2; i > -1; i--) {
+            originalArray[i] = arr[i] - originalArray[i + 1];
         }
 
-        // System.out.println("Original Array:");
-        // for (int i : originalArray) {
-        //     System.out.print(i + "\t");
-        // }
-        // System.out.println();
+        System.out.println("Original Array:");
+        for (int i : originalArray) {
+            System.out.print(i + "\t");
+        }
+        System.out.println();
 
         for (int i : originalArray) {
             sum += i;
@@ -43,8 +44,8 @@ public class SimpleEncodedArray {
     }
 
     public static void main(String[] args) {
-        new SimpleEncodedArray().findOriginalFirstAndSum(new int[] { 7, 6, 8, 16, 12, 3 }, 6);
+        // new SimpleEncodedArray().findOriginalFirstAndSum(new int[] { 7, 6, 8, 16, 12, 3 }, 6);
         new SimpleEncodedArray().findOriginalFirstAndSum(new int[] { -2, -7, -12, -15 }, 4);
-        new SimpleEncodedArray().findOriginalFirstAndSum(new int[] { -10, 10 }, 2);
+        // new SimpleEncodedArray().findOriginalFirstAndSum(new int[] { -10, 10 }, 2);
     }
 }
